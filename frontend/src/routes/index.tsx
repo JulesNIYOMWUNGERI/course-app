6a21994005f3054de6e796d22b9f953c80b3266c
kayigmb <kayigm1@gmail.com>
2025-06-18 10:49:00 +0200

    ft/setup_backend

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
+import {useState} from "react";
+import {createBrowserRouter, Navigate, Outlet} from "react-router-dom";
 
-import { Navbar, Sidebar } from "../components";
+import {Navbar, Sidebar} from "../components";
 import CourseLayout from "./CourseLayout";
 import Administration from "../pages/Administration/Administration";
 import CourseDetailsOverview from "../pages/Course/CourseDetailsOverview/CourseDetailsOverview";
@@ -10,60 +10,65 @@ import CourseOverview from "../pages/Course/CourseOverview/CourseOverview";
 import Home from "../pages/Home/Home";
 
 const Layout = () => {
-  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+    const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
 
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
 
-  return (
-    <div className="container">
-      <Sidebar sidebarOpen={sidebarOpen} onClick={toggleSidebar} />
-      <>
-        <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        <Outlet />
-      </>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Sidebar sidebarOpen={sidebarOpen} onClick={toggleSidebar}/>
+            <>
+                <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+                <Outlet/>
+            </>
+        </div>
+    );
 };
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Navigate to="/home" />,
-      },
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/administration",
-        element: <Administration />,
-      },
-      {
-        path: "/course",
-        element: <CourseLayout />,
-        children: [
-          {
-            path: "management",
-            element: <CourseManagement />,
-          },
-          {
-            path: "overview",
-            element: <CourseOverview />,
-          },
-          {
-            path: "details/:id",
-            element: <CourseDetailsOverview />,
-          },
-        ],
-      },
+        {
+            path: "/",
+            element: <Layout/>,
+            children: [
+                {
+                    path: "/",
+                    element: <Navigate to="/home"/>,
+                },
+                {
+                    path: "/home",
+                    element: <Home/>,
+                },
+                {
+                    path: "/administration",
+                    element: <Administration/>,
+                },
+                {
+                    path: "/course",
+                    element: <CourseLayout/>,
+                    children: [
+                        {
+                            path: "management",
+                            element: <CourseManagement/>,
+                        },
+                        {
+                            path: "overview",
+                            element: <CourseOverview/>,
+                        },
+                        {
+                            path: "details/:id",
+                            element: <CourseDetailsOverview/>,
+                        },
+                    ],
+                },
+            ],
+        },
     ],
-  },
-]);
+
+    {
+        basename: "/home"
+    }
+);
 
 export default router;
