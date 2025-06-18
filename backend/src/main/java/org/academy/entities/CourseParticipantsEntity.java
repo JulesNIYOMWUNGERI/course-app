07c9f814e64609e0725b9746254e933bb570584e
kayigmb <kayigm1@gmail.com>
2025-06-18 11:37:35 +0200

    ft/add_courseparticipants

---
+++ backend/src/main/java/org/academy/entities/CourseParticipantsEntity.java
@@ -0,0 +1,31 @@
+package org.academy.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course_participants", schema = "course_app")
+public class CourseParticipantsEntity extends BaseEntity {
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "course_id", nullable = false)
+    private CourseEntity course;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    public CourseEntity getCourse() {
+        return course;
+    }
+
+    public void setCourse(CourseEntity course) {
+        this.course = course;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
