ab1f2f5ac57295e07b013a82ef8ba3a2ab7183de
Jules NIYOMWUNGERI <julesniyomwungeri144@gmail.com>
2025-06-23 11:22:37 +0200

    fixing meta modals and loading issues

--- src/main/java/org/academy/repositories/UserRepository.java
+++ src/main/java/org/academy/repositories/UserRepository.java
@@ -7,57 +7,57 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-import org.academy.entities.BaseEntity_;
-import org.academy.entities.UserEntity;
-
 import java.util.List;
 import java.util.UUID;
+import org.academy.entities.BaseEntity_;
+import org.academy.entities.UserEntity;
+import org.academy.entities.UserEntity_;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<UserEntity> {
-    public List<UserEntity> getAllUsers() {
-        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<UserEntity> cq = cb.createQuery(UserEntity.class);
-        Root<UserEntity> user = cq.from(UserEntity.class);
+  public List<UserEntity> getAllUsers() {
+    CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+    CriteriaQuery<UserEntity> cq = cb.createQuery(UserEntity.class);
+    Root<UserEntity> user = cq.from(UserEntity.class);
 
-        cq.select(user);
-        cq.orderBy(cb.desc(user.get("createdAt")));
+    cq.select(user);
+    cq.orderBy(cb.desc(user.get(UserEntity_.createdAt)));
 
-        return getEntityManager().createQuery(cq).getResultList();
+    return getEntityManager().createQuery(cq).getResultList();
+  }
+
+  public UserEntity findByUsername(String firstName, String lastName) {
+    CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+    CriteriaQuery<UserEntity> cq = cb.createQuery(UserEntity.class);
+    Root<UserEntity> user = cq.from(UserEntity.class);
+
+    Predicate firstNamePredicate = cb.equal(user.get(UserEntity_.firstName), firstName);
+    Predicate lastNamePredicate = cb.equal(user.get(UserEntity_.lastName), lastName);
+
+    cq.select(user).where(cb.and(firstNamePredicate, lastNamePredicate));
+
+    try {
+      return getEntityManager().createQuery(cq).getSingleResult();
+    } catch (NoResultException e) {
+      return null;
     }
+  }
 
-    public UserEntity findByUsername(String firstName, String lastName) {
-        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<UserEntity> cq = cb.createQuery(UserEntity.class);
-        Root<UserEntity> user = cq.from(UserEntity.class);
+  public UserEntity findUserById(UUID id) {
+    CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+    CriteriaQuery<UserEntity> cq = cb.createQuery(UserEntity.class);
+    Root<UserEntity> user = cq.from(UserEntity.class);
 
-        Predicate firstNamePredicate = cb.equal(user.get("firstName"), firstName);
-        Predicate lastNamePredicate = cb.equal(user.get("lastName"), lastName);
+    cq.select(user).where(cb.equal(user.get(BaseEntity_.ID), id));
 
-        cq.select(user).where(cb.and(firstNamePredicate, lastNamePredicate));
-
-        try {
-            return getEntityManager().createQuery(cq).getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
+    try {
+      return getEntityManager().createQuery(cq).getSingleResult();
+    } catch (NoResultException e) {
+      return null;
     }
+  }
 
-    public UserEntity findUserById(UUID id) {
-        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<UserEntity> cq = cb.createQuery(UserEntity.class);
-        Root<UserEntity> user = cq.from(UserEntity.class);
-
-        cq.select(user).where(cb.equal(user.get(BaseEntity_.ID), id));
-
-        try {
-            return getEntityManager().createQuery(cq).getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
-    }
-
-    public void merge(UserEntity user) {
-        getEntityManager().merge(user);
-    }
+  public void merge(UserEntity user) {
+    getEntityManager().merge(user);
+  }
 }
