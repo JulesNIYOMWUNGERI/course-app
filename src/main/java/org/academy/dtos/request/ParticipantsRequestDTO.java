1647c3acc5e72a817125258595fd2367158053db
Jules NIYOMWUNGERI <julesniyomwungeri144@gmail.com>
2025-06-26 11:27:08 +0200

    fixing style issues

--- src/main/java/org/academy/dtos/request/ParticipantsRequestDTO.java
+++ src/main/java/org/academy/dtos/request/ParticipantsRequestDTO.java
@@ -2,10 +2,8 @@ package org.academy.dtos.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-
 import java.util.UUID;
 
 public record ParticipantsRequestDTO(
-        @NotNull(message = "Course Id is required") @Valid UUID courseId,
-        @NotNull(message = "User Id is required") @Valid UUID userId) {
-}
+    @NotNull(message = "Course Id is required") @Valid UUID courseId,
+    @NotNull(message = "User Id is required") @Valid UUID userId) {}
--- src/main/java/org/academy/services/CourseService.java
+++ src/main/java/org/academy/services/CourseService.java
@@ -3,6 +3,8 @@ package org.academy.services;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
 import org.academy.dtos.PaginationDto;
 import org.academy.dtos.request.CourseRequestDTO;
 import org.academy.dtos.response.CourseDTO;
@@ -13,68 +15,64 @@ import org.academy.exceptions.NotFounderException;
 import org.academy.mappers.CourseMapper;
 import org.academy.repositories.CourseRepository;
 
-import java.util.List;
-import java.util.UUID;
-
 @ApplicationScoped
 public class CourseService {
-    @Inject
-    CourseRepository courseRepository;
+  @Inject CourseRepository courseRepository;
 
-    public PaginationDto<List<CourseDTO>> getCourses(
-            int page, int size, String name, String department, String classification) {
-        CoursePaginatedResponseDTO<CourseEntity> result =
-                courseRepository.findCourses(page, size, name, department, classification);
+  public PaginationDto<List<CourseDTO>> getCourses(
+      int page, int size, String name, String department, String classification) {
+    CoursePaginatedResponseDTO<CourseEntity> result =
+        courseRepository.findCourses(page, size, name, department, classification);
 
-        List<CourseDTO> data = result.getItems().stream().map(CourseMapper::toDTO).toList();
+    List<CourseDTO> data = result.getItems().stream().map(CourseMapper::toDTO).toList();
 
-        long totalCount = result.getTotalCount();
-        long totalPages = (long) Math.ceil((double) totalCount / size);
-        return new PaginationDto<>(totalCount, totalPages, page, size, data);
+    long totalCount = result.getTotalCount();
+    long totalPages = (long) Math.ceil((double) totalCount / size);
+    return new PaginationDto<>(totalCount, totalPages, page, size, data);
+  }
+
+  public CourseFullResponseDTO getCourseById(UUID id) {
+    CourseEntity course = courseRepository.find("id", id).firstResult();
+    if (course == null) {
+      throw new NotFounderException("Course not found with ID: " + id);
+    }
+    return CourseMapper.toFullDTO(course);
+  }
+
+  @Transactional
+  public CourseDTO createCourse(CourseRequestDTO course) throws NotFounderException {
+    CourseEntity existingCourse = courseRepository.find("name", course.name()).firstResult();
+    if (existingCourse != null) {
+      throw new NotFounderException("Course with the same name already exists");
     }
 
-    public CourseFullResponseDTO getCourseById(UUID id) {
-        CourseEntity course = courseRepository.find("id", id).firstResult();
-        if (course == null) {
-            throw new NotFounderException("Course not found with ID: " + id);
-        }
-        return CourseMapper.toFullDTO(course);
+    CourseEntity entity = CourseMapper.toEntity(course);
+    courseRepository.persist(entity);
+    return CourseMapper.toDTO(entity);
+  }
+
+  @Transactional
+  public CourseDTO updateCourse(UUID id, CourseRequestDTO course) throws NotFounderException {
+    CourseEntity existingCourse = courseRepository.find("id", id).firstResult();
+    if (existingCourse == null) {
+      throw new NotFounderException("Course not found with ID: " + id);
     }
 
-    @Transactional
-    public CourseDTO createCourse(CourseRequestDTO course) throws NotFounderException {
-        CourseEntity existingCourse = courseRepository.find("name", course.name()).firstResult();
-        if (existingCourse != null) {
-            throw new NotFounderException("Course with the same name already exists");
-        }
+    existingCourse.setName(course.name());
+    existingCourse.setNumberOfParticipants(course.numberOfParticipants());
+    existingCourse.setParticipantsGroup(course.participantsGroup());
+    existingCourse.setClassification(course.classification());
+    existingCourse.setDepartment(course.department());
+    return CourseMapper.toDTO(existingCourse);
+  }
 
-        CourseEntity entity = CourseMapper.toEntity(course);
-        courseRepository.persist(entity);
-        return CourseMapper.toDTO(entity);
+  @Transactional
+  public String removeCourseById(UUID id) throws NotFounderException {
+    CourseEntity course = courseRepository.findById(id);
+    if (course == null) {
+      throw new NotFounderException("Course not found with ID: " + id);
     }
-
-    @Transactional
-    public CourseDTO updateCourse(UUID id, CourseRequestDTO course) throws NotFounderException {
-        CourseEntity existingCourse = courseRepository.find("id", id).firstResult();
-        if (existingCourse == null) {
-            throw new NotFounderException("Course not found with ID: " + id);
-        }
-
-        existingCourse.setName(course.name());
-        existingCourse.setNumberOfParticipants(course.numberOfParticipants());
-        existingCourse.setParticipantsGroup(course.participantsGroup());
-        existingCourse.setClassification(course.classification());
-        existingCourse.setDepartment(course.department());
-        return CourseMapper.toDTO(existingCourse);
-    }
-
-    @Transactional
-    public String removeCourseById(UUID id) throws NotFounderException {
-        CourseEntity course = courseRepository.findById(id);
-        if (course == null) {
-            throw new NotFounderException("Course not found with ID: " + id);
-        }
-        courseRepository.delete(course);
-        return "Course with ID " + id + " has been removed successfully.";
-    }
+    courseRepository.delete(course);
+    return "Course with ID " + id + " has been removed successfully.";
+  }
 }
