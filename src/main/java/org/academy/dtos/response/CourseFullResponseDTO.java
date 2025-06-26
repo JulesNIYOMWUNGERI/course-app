065239c65447a8202e93b83b93b6c72f4b399500
Jules NIYOMWUNGERI <julesniyomwungeri144@gmail.com>
2025-06-26 11:27:08 +0200

    fixing style issues

--- src/main/java/org/academy/dtos/response/CourseFullResponseDTO.java
+++ src/main/java/org/academy/dtos/response/CourseFullResponseDTO.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 public record CourseFullResponseDTO(
-        UUID id,
-        String name,
-        Integer numberOfParticipants,
-        String classification,
-        String department,
-        List<String> participantsGroup,
-        List<CourseParticipantsUserDTO> courseParticipants,
-        List<CourseDocumentsResponseDTO> courseDocuments) {
-}
+    UUID id,
+    String name,
+    Integer numberOfParticipants,
+    String classification,
+    String department,
+    List<String> participantsGroup,
+    List<CourseParticipantsUserDTO> courseParticipants,
+    List<CourseDocumentsResponseDTO> courseDocuments) {}
